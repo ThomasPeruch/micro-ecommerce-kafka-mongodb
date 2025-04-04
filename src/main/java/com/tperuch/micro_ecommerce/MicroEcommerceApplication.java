4f1a41581e69970e53aebfcfe91bf65ac6b0db92
Thomas Peruch <peruchthomas@gmail.com>
2025-04-04 18:56:38 -0300

    initial commit

---
+++ src/main/java/com/tperuch/micro_ecommerce/MicroEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.tperuch.micro_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tperuch/micro_ecommerce/MicroEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tperuch.micro_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
